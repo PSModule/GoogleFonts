4eba7ec5dcfe3f9bdb2aed7b717cc79a65006089
Marius Storhaug <marstor@hotmail.com>
2024-03-22 22:55:10 +0100

    Init

---
+++ tests/GoogleFonts.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'GoogleFonts' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'GoogleFonts' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'GoogleFonts' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be importable' {
+            { Import-Module -Name 'GoogleFonts' } | Should -Not -Throw
+        }
+    }
+}
