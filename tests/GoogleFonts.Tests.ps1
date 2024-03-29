1755e31978c6507c499be7d7b0c0200466974921
Marius Storhaug <marstor@hotmail.com>
2024-03-29 02:00:15 +0100

    🩹 [Patch]: Align code and process (#2)
    ## Description
    
    🩹 [Patch]: Align code and process
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/GoogleFonts.Tests.ps1
+++ tests/GoogleFonts.Tests.ps1
@@ -1,11 +1,20 @@
-﻿Describe 'GoogleFonts' {
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Write-Verbose "Path to the module: [$Path]" -Verbose
+
+Describe 'GoogleFonts' {
     Context 'Module' {
         It 'The module should be available' {
             Get-Module -Name 'GoogleFonts' -ListAvailable | Should -Not -BeNullOrEmpty
             Write-Verbose (Get-Module -Name 'GoogleFonts' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be importable' {
-            { Import-Module -Name 'GoogleFonts' } | Should -Not -Throw
+            { Import-Module -Name 'GoogleFonts' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
         }
     }
 }
