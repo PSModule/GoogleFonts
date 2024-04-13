1ff47c8aa6f76ecb2e33602f5749368a1a748cb6
Marius Storhaug <marstor@hotmail.com>
2024-04-13 23:44:04 +0200

    Remove import test

--- tests/GoogleFonts.Tests.ps1
+++ tests/GoogleFonts.Tests.ps1
@@ -8,13 +8,4 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'GoogleFonts' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'GoogleFonts' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'GoogleFonts' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be importable' {
-            { Import-Module -Name 'GoogleFonts' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
 }
