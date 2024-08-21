4cebc0cb8b80c2dbcfca9233d32a8b27909e4790
Marius Storhaug <marstor@hotmail.com>
2024-08-22 00:02:39 +0200

    🚀 [Feature]: Restructure for Process-PSModule v3 (#10)
    ## Description
    
    - Removing support for Windows PowerShell - directional change for
    PSModule.
    - Remove nuget from Dependabot settings
    - Using Process-PSModule v3
    - Restructure for v3
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/GoogleFonts.Tests.ps1
+++ tests/GoogleFonts.Tests.ps1
@@ -8,4 +8,12 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'GoogleFonts' {
+    Context 'Get-PSModule' {
+        It 'should greet the world' {
+            $Name = 'World'
+            $Expected = "Hello, $Name!"
+            $Actual = Get-PSModule -Name $Name
+            $Actual | Should -Be $Expected
+        }
+    }
 }
