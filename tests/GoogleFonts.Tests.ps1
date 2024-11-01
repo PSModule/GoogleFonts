bb1978cbd640973e04b95ae14e13e82c22bd416d
Marius Storhaug <marstor@hotmail.com>
2024-11-02 00:16:08 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#11)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/GoogleFonts.Tests.ps1
+++ tests/GoogleFonts.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'GoogleFonts' {
+﻿Describe 'GoogleFonts' {
     Context 'Get-PSModule' {
         It 'should greet the world' {
             $Name = 'World'
