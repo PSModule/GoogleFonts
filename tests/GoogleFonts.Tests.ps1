3c5aede6c74d3b1f2a29fe6bb0141e5ae6f1e3bb
Marius Storhaug <marstor@hotmail.com>
2025-01-12 23:37:28 +0100

    🩹 [Patch]: Update tests for Google Fonts functionality, adding cases for Get-GoogleFont and Install-GoogleFont

--- tests/GoogleFonts.Tests.ps1
+++ tests/GoogleFonts.Tests.ps1
@@ -1,10 +1,27 @@
-﻿Describe 'GoogleFonts' {
-    Context 'Get-PSModule' {
-        It 'should greet the world' {
-            $Name = 'World'
-            $Expected = "Hello, $Name!"
-            $Actual = Get-PSModule -Name $Name
-            $Actual | Should -Be $Expected
+﻿Describe 'Module' {
+    Context 'Function: Get-GoogleFont' {
+        It 'Returns all fonts' {
+            $fonts = Get-GoogleFont
+            Write-Verbose ($fonts | Out-String) -Verbose
+            $fonts | Should -Not -BeNullOrEmpty
+        }
+
+        It 'Returns a specific font' {
+            $font = Get-GoogleFont -Name 'Roboto'
+            Write-Verbose ($font | Out-String) -Verbose
+            $font | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context 'Function: Install-GoogleFont' {
+        It '[Install-GoogleFont] - Installs a font' {
+            { Install-GoogleFont -Name 'Akshar' } | Should -Not -Throw
+            Get-Font -Name 'Akshar*' | Should -Not -BeNullOrEmpty
+        }
+
+        It '[Install-GoogleFont] - Installs all fonts' {
+            { Install-GoogleFont -All -Verbose } | Should -Not -Throw
+            Get-Font -Name 'Nabla*' | Should -Not -BeNullOrEmpty
         }
     }
 }
