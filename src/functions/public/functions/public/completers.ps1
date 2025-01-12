fb530400b0601b9eed2e4e86bf0ad063dc26f631
Marius Storhaug <marstor@hotmail.com>
2025-01-12 23:36:03 +0100

    🩹 [Patch]: Ensure unique font names and variants in argument completers for Google Fonts

--- src/functions/public/functions/public/completers.ps1
+++ src/functions/public/functions/public/completers.ps1
@@ -1,7 +1,7 @@
 ﻿Register-ArgumentCompleter -CommandName Get-GoogleFont, Install-GoogleFont -ParameterName Name -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
-    Get-GoogleFont | Select-Object -ExpandProperty Name | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+    Get-GoogleFont | Select-Object -ExpandProperty Name | Sort-Object -Unique | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
@@ -9,8 +9,8 @@
 Register-ArgumentCompleter -CommandName Get-GoogleFont, Install-GoogleFont -ParameterName Variant -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
-    Get-GoogleFont -Name $fakeBoundParameter.Name | Select-Object -ExpandProperty Variant | Where-Object { $_ -like "$wordToComplete*" } |
-        ForEach-Object {
+    Get-GoogleFont -Name $fakeBoundParameter.Name | Select-Object -ExpandProperty Variant | Sort-Object -Unique |
+        Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
 }
