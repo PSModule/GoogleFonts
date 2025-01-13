d0e468c248cf0c2f7a740e05961084e6e6b9933d
Marius Storhaug <marstor@hotmail.com>
2025-01-13 01:07:30 +0100

    Remove unnecessary conversion from JSON in GoogleFonts.ps1

--- src/variables/private/GoogleFonts.ps1
+++ src/variables/private/GoogleFonts.ps1
@@ -1,2 +1,1 @@
-﻿$script:GoogleFonts = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/PSModule/GoogleFonts/refs/heads/main/data/GoogleFonts.json' |
-    ConvertFrom-Json
+﻿$script:GoogleFonts = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/PSModule/GoogleFonts/refs/heads/main/data/GoogleFonts.json'
