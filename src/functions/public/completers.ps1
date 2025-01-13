595387e67093d000b97c506e35a0ebc4666c836e
Marius Storhaug <marstor@hotmail.com>
2025-01-13 23:26:01 +0100

    Fix argument completer pattern matching in completers.ps1 for accurate autocomplete results

--- src/functions/public/completers.ps1
+++ src/functions/public/completers.ps1
@@ -1,7 +1,7 @@
 ﻿Register-ArgumentCompleter -CommandName Get-GoogleFont, Install-GoogleFont -ParameterName Name -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
-    Get-GoogleFont -Verbose:$false | Select-Object -ExpandProperty Name | Sort-Object -Unique | Where-Object { $_ -like "*$wordToComplete*" } |
+    Get-GoogleFont -Verbose:$false | Select-Object -ExpandProperty Name | Sort-Object -Unique | Where-Object { $_ -like "$wordToComplete*" } |
         ForEach-Object {
             [System.Management.Automation.CompletionResult]::new("'$_'", $_, 'ParameterValue', $_)
         }
